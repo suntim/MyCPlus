05bf0821304e74786d60c6cc81e1d2a1037a1b59
Alex_XT <1056294025@qq.com>
2018-05-19 13:32:26 +0000

    Create main.cpp

---
+++ 023_JoseClass/main.cpp
@@ -0,0 +1,21 @@
+// 023_JoseClass.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include"josex.h"
+#include<iostream>
+using namespace std;
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Jose j;
+	
+	j.Initial();
+
+	
+	j.GetWinner();
+	return 0;
+}
+
