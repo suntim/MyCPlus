3785b808213e3282680282a13b3bc2fc474ba6b7
Alex_XT <1056294025@qq.com>
2018-05-20 08:40:05 +0000

    Create main.cpp

---
+++ 024_JoseClassMoreWinner/main.cpp
@@ -0,0 +1,18 @@
+// 024_JoseClassMoreWiners.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include"josex.h"
+#include"ring.h"
+#include<iostream>
+using namespace std;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	Jose j;
+	j.Initial();
+	j.GetWinner();
+	return 0;
+}
+
